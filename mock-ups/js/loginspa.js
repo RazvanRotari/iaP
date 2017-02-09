3900880f97f760a885ba4606a4ea95b742b62a49
stefanoprea <stefanoprea128@gmail.com>
2017-02-09 15:33:09 +0200

    bigger is better

--- mock-ups/js/loginspa.js
+++ mock-ups/js/loginspa.js
@@ -1,13 +1,56 @@
-(function(){
-    for(var ev of document.getElementsByClassName("login-button"))
-        ev.addEventListener("click",function(){
-            username=document.getElementsByName("username")[0].value;
-            password=document.getElementsByName("password")[0].value;
-            if(username==="")
-                alert("Please enter a username");
-            else if(password==="")
-                alert("Please enter a password");
-            else 
-                alert("Your username is: "+username+" and your password is: "+password);
-        });
+//beautified with jsbeautifier from pip
+(function() {
+
+    function assert(v) {
+        if (!v) {
+            throw Error("assert error");
+        }
+    }
+
+    function formPage(name, formNames) {
+        this.name = name
+        this.formNames = formNames
+    }
+    formPage.prototype = {
+        submit: function() {
+            var p = document.getElementsByClassName(this.name);
+            assert(p.length == 1)
+            p = p[0]
+            r = {}
+            for (var fname of this.formNames) {
+                el = p.getElementsByClassName(fname);
+                assert(el.length == 1)
+                el = el[0]
+                var v = el.value.trim()
+                if (!v) {
+                    this.print("Please fill out " + el.name + " field.");
+                    return;
+                }
+                r[fname] = v;
+            }
+            alert(r);
+        },
+        print: function(x) {
+            alert(x)
+        },
+    };
+
+    for (var ev of document.getElementsByClassName("data-login-button")) {
+        (function() {
+            var x = new formPage("data-login", ["data-username", "data-password", ]);
+            ev.addEventListener("click", function() {
+                x.submit();
+            });
+        })();
+    }
+
+    for (var ev of document.getElementsByClassName("data-register-button")) {
+        (function() {
+            var x = new formPage("data-register", ["data-name", "data-email", "data-username", "data-password", "data-confirm-password", ]);
+            ev.addEventListener("click", function() {
+                x.submit();
+            });
+        })();
+    }
+
 })();
